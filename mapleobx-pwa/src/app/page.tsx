a75e89f16ed89517fd2a69c4a52938c31711284d
Final Hyen <jwx9797@gmail.com>
2024-03-05 16:16:24 +0900

    feat: Home layout, page

--- mapleobx-pwa/src/app/page.tsx
+++ mapleobx-pwa/src/app/page.tsx
@@ -1,5 +1,18 @@
+"use client";
 import Image from "next/image";
-
+import search from "/public/img/search.webp";
 export default function Home() {
-  return <>Hello, MapBox!</>;
+  return (
+    <div className="w-full flex flex-row items-center">
+      <input
+        className="flex-grow h-auto text-lg bg-[#EBEBEB] border-solid border-1 border-black pl-2"
+        type="text"
+        name="nickName"
+        placeholder="캐릭터 닉네임을 입력해주세요"
+      />
+      <button>
+        <Image width="20" height="20" src={search} alt="돋보기 이미지" />
+      </button>
+    </div>
+  );
 }
