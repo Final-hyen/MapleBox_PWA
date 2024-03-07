18be73a11c9eb3d5cbd3f6eaeea4c05d53d4ed94
Final Hyen <jwx9797@gmail.com>
2024-03-07 16:53:17 +0900

    style: Home CSS

--- mapleobx-pwa/src/app/layout.tsx
+++ mapleobx-pwa/src/app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from "next";
 import Link from "next/link";
+import Image from "next/image";
 import "./globals.css";
+import Mark from "/public/img/Mark.png";
 
 export const metadata: Metadata = {
   title: "Maple Cube",
@@ -17,8 +19,15 @@ export default function RootLayout({
       <body>
         <header className="my-2 h-[50px] w-full bg-white">
           <ol className="flex h-full items-center justify-between">
-            <li className="m1-16 flex h-full items-center gap-[45px] text-[20px]">
-              <Link className="font-bold" href={`/`}>Maple Cube</Link>
+            <li className="m1-16 flex h-full items-center gap-[45px] text-[21px]">
+              <Link className="flex flex-row font-bold items-center" href={`/`}>
+                <Image
+                  className="w-[48px] h-[48px] m-[10px]"
+                  src={Mark}
+                  alt="마크 이미지"
+                />
+                Cuoble
+              </Link>
               <Link className="mr-10 hover:text-blue-600" href={`/result`}>
                 결과
               </Link>
@@ -29,11 +38,12 @@ export default function RootLayout({
           </ol>
         </header>
         {children}
-        <footer className="text-center text-[18px] bg-[#A5DD9B] text-gray-50 h-[100px]">
-          Copyright 2024. 스카니아@풀타수집, all rights reserved. Cuogle is not
-          associated with NEXON Korea.
-          <br />
-          contact: jwx9797@gmail.com
+        <footer className="flex flex-col items-center justify-center text-center text-[18px] bg-[#A5DD9B] text-gray-50 h-[100px]">
+          <p>
+            Copyright 2024. 스카니아@풀타수집, all rights reserved. Cuogle is
+            not associated with NEXON Korea.
+          </p>
+          <p>contact: jwx9797@gmail.com</p>
         </footer>
       </body>
     </html>
--- mapleobx-pwa/src/app/page.tsx
+++ mapleobx-pwa/src/app/page.tsx
@@ -1,26 +1,32 @@
 "use client";
 import Image from "next/image";
 import search from "/public/img/search.webp";
+
 export default function Home() {
   return (
-    <div className="flex-1 h-full bg-[#C5EBAA]">
-      <div className="flex justify-between lg:justify-center">
+    <div className="h-[980px] flex justify-center bg-[#C5EBAA]">
+      <div className="flex justify-center items-center">
         <div className="flex w-[500px] flex-col gap-[13px] rounded-[10px] p-[30px]">
-          <div className="flex w-full flex-col gap-[8px] px-[48px] mo:px-[24px]">
+          <h2 className="text-center font-bold text-[72px]">Couble</h2>
+          <div className="flex w-full flex-col gap-[8px]">
             <div className="relative">
               <input
-                className="flex-grow w-full h-[44px] rounded-[2px] bg-[#EBEBEB] p-[15px_30px_15px_12px]"
+                className="flex-grow w-full h-[52px] rounded-[2px] bg-[#EBEBEB] p-[15px_30px_15px_12px]"
                 type="text"
                 name="nickName"
                 placeholder="캐릭터 닉네임을 입력해주세요"
               />
               <button>
                 <Image
-                  className="absolute right-[13px] top-[13px] w-[18px]"
+                  className="absolute right-[13px] top-[13px] w-[25px]"
                   src={search}
                   alt="돋보기 이미지"
                 />
               </button>
+            </div>
+            <div className="m-[5px] text-center text-[15px] font-semibold">
+              <p className="text-red-500">* 넥슨 API 데이터는 어제까지의 정보만 제공됩니다.</p>
+              <p>* 닉네임은 대소문자 구분이 필요합니다.</p>
             </div>
           </div>
         </div>
