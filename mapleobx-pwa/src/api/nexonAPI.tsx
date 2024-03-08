9c4c2d2a7673da529966b70665e7dfd54227c2d9
Final Hyen <jwx9797@gmail.com>
2024-03-08 20:44:39 +0900

    feat: getBasicInfo function

--- mapleobx-pwa/src/api/nexonAPI.tsx
+++ mapleobx-pwa/src/api/nexonAPI.tsx
@@ -1,24 +1,52 @@
 //NEXON OpenAPI
+import { dateState } from "@/recoil/atom";
 import axios from "axios";
+import { useRecoilValue } from "recoil";
+
 interface Ocid {
   ocid: string;
 }
 
 export async function getOcid(nickname: string) {
-    try {
-      const res = await axios.get<Ocid>(
-        'https://open.api.nexon.com/maplestory/v1/id', 
-        { 
-          params: { character_name: nickname }, 
-          headers: { 
-            "x-nxopen-api-key": "test_c01ef821dcec1e7253351260a8f3a6f4d671b40b3971b399bb2b8302cee5b3c69f471f45aec7b0b56254304f6850106c"
-          } 
-        }
-      );
-      const data = res.data;
-      console.log(data);
-      return data;
-    } catch (error) {
-      console.log(error);
-    }
+  try {
+    const res = await axios.get<Ocid>(
+      "https://open.api.nexon.com/maplestory/v1/id",
+      {
+        params: { character_name: nickname },
+        headers: {
+          "x-nxopen-api-key":
+            "test_c01ef821dcec1e7253351260a8f3a6f4d671b40b3971b399bb2b8302cee5b3c69f471f45aec7b0b56254304f6850106c",
+        },
+      }
+    );
+    const data = res.data;
+    localStorage.setItem("ocid", JSON.stringify(data.ocid));
+
+    return data;
+  } catch (error) {
+    console.log(error);
   }
+}
+
+export async function getBasicInfo(
+  ocid: string,
+  date: string,
+  dateStateValue: string
+) {
+  try {
+    const res = await axios.get(
+      "https://open.api.nexon.com/maplestory/v1/character/basic",
+      {
+        params: { ocid: localStorage.getItem("ocid"), date: dateStateValue },
+        headers: {
+          "x-nxopen-api-key":
+            "test_c01ef821dcec1e7253351260a8f3a6f4d671b40b3971b399bb2b8302cee5b3c69f471f45aec7b0b56254304f6850106c",
+        },
+      }
+    );
+    const data = res.data;
+    console.log(data);
+  } catch (error) {
+    console.log(error);
+  }
+}
--- mapleobx-pwa/src/app/page.tsx
+++ mapleobx-pwa/src/app/page.tsx
@@ -2,44 +2,60 @@
 import Image from "next/image";
 import search from "/public/img/search.webp";
 import { getOcid } from "@/api/nexonAPI";
-import { FormEvent, useState } from "react";
+import { FormEvent, useEffect, useState } from "react";
+import moment from "moment";
+import { useRecoilState } from "recoil";
+import { dateState } from "@/recoil/atom";
+
 export default function Home() {
-  const [name, setName] = useState<string>("")
+  const [name, setName] = useState<string>("");
+  const [date, setDate] = useRecoilState(dateState);
   const isClickSearch = (e: FormEvent) => {
     e.preventDefault();
     getOcid(name);
   };
+  console.log(date, name);
+  useEffect(() => {
+    const today = moment().format("YYYY-MM-DD");
+    setDate(today);
+  }, [setDate]);
+
   return (
-    <div className="h-[980px] flex justify-center bg-[#C5EBAA]">
-      <div className="flex justify-center items-center">
-        <div className="flex w-[500px] flex-col gap-[13px] rounded-[10px] p-[30px]">
-          <h2 className="text-center font-bold text-[72px]">Couble</h2>
-          <div className="flex w-full flex-col gap-[8px]">
+    <div className="h-[980px] flex justify-center items-center bg-[#C5EBAA]">
+      <div className="flex w-[500px] flex-col gap-[13px] rounded-[10px] p-[30px]">
+        <h2 className="text-center font-bold text-[72px]">Couble</h2>
+        <div className="flex w-full flex-col gap-[8px]">
+          <form onSubmit={isClickSearch}>
+            <input
+              type="date"
+              value={date}
+              onChange={(e) => setDate(e.target.value)}
+            ></input>
             <div className="relative">
-              <form onSubmit={isClickSearch}>
-                <input
-                  className="flex-grow w-full h-[52px] rounded-[2px] bg-[#EBEBEB] p-[15px_30px_15px_12px]"
-                  type="text"
-                  name="name"
-                  onChange={(e) => {setName(e.target.value)}}
-                  placeholder="캐릭터 닉네임을 입력해주세요"
+              <input
+                className="flex-grow w-full h-[52px] rounded-[2px] bg-[#EBEBEB] p-[15px_30px_15px_12px]"
+                type="text"
+                name="name"
+                onChange={(e) => {
+                  setName(e.target.value);
+                }}
+                placeholder="캐릭터 닉네임을 입력해주세요"
+              />
+              <button type="submit">
+                <Image
+                  className="absolute right-[13px] top-[13px] w-[25px]"
+                  src={search}
+                  alt="돋보기 이미지"
                 />
-                <button type="submit">
-                  <Image
-                    className="absolute right-[13px] top-[13px] w-[25px]"
-                    src={search}
-                    alt="돋보기 이미지"
-                  />
-                </button>
-              </form>
+              </button>
             </div>
-            <div className="m-[5px] text-center text-[15px] font-semibold">
-              <p className="text-red-500">
-                * 넥슨 API 데이터는 어제까지의 정보만 제공됩니다.
-              </p>
-              <p>* 닉네임은 대소문자 구분이 필요합니다.</p>
-            </div>
-          </div>
+          </form>
+        </div>
+        <div className="m-[5px] text-center text-[15px] font-semibold">
+          <p className="text-red-500">
+            * 넥슨 API 데이터는 어제까지의 정보만 제공됩니다.
+          </p>
+          <p>* 닉네임은 대소문자 구분이 필요합니다.</p>
         </div>
       </div>
     </div>
